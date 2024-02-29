86145bc0bea664203fbfcad746b27fd994146c19
Yovel <yovelnir@gmail.com>
2024-02-29 22:17:03 +0200

    Add project files.

---
+++ LiquorLand/Areas/Identity/Data/UserDbContext.cs
@@ -0,0 +1,22 @@
+﻿using LiquorLand.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LiquorLand.Data;
+
+public class UserDbContext : IdentityDbContext<Users>
+{
+    public UserDbContext(DbContextOptions<UserDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
---
+++ LiquorLand/Areas/Identity/Data/Users.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+#nullable disable
+
+namespace LiquorLand.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the Users class
+public class Users : IdentityUser
+{ 
+    [PersonalData]
+    [Required]
+    [Column(TypeName = "nvarchar(100)")]
+    public string FirstName { get; set; }
+
+    [PersonalData]
+    [Required]
+    [Column(TypeName = "nvarchar(100)")]
+    public string LastName { get; set; }
+}
+
